064434ee0dcec9fb8c035ca123af3c995693696f
PEGutierrezF <pabloe.gutierrezfonseca@gmail.com>
2025-02-05 21:25:28 -0500

    class

--- Module 4 Descriptive Statistic/M4 Group  Work/Group work.R
+++ Module 4 Descriptive Statistic/M4 Group  Work/Group work.R
@@ -21,6 +21,41 @@ head(snowdata)
 pivot(snowdata, c(mean, min,max,var,sd,skew,kurtosis,IQR,quantile), Mean_Dec_May_snow_depth)
 ?pivot
 
+#this quantile function is from lessR
+quartiles <- quantile(snowdata$Mean_Dec_May_snow_depth, probs = c(0.25,0.5,0.75))
+
+#these ggplot functions are from the tidyverse
+ggplot(snowdata, aes(x = "", y = Mean_Dec_May_snow_depth)) + 
+  geom_boxplot() + 
+  stat_summary(fun.y = mean, geom = "point", 
+               shape = 18, size = 3, color = "red") +
+  geom_jitter(position=position_jitter(0.2)) +
+  theme_bw()
+
+
+# Quantiles ---------------------------------------------------------------
+
+
+# Calculate quartiles and store them in a data frame
+quartiles <- data.frame(
+  y = quantile(snowdata$Mean_Dec_May_snow_depth, probs = c(0.25, 0.5, 0.75)),
+  label = c("Q1", "Median", "Q3")
+)
+
+ggplot(snowdata, aes(x = "", y = Mean_Dec_May_snow_depth)) + 
+  geom_boxplot() + 
+  stat_summary(fun.y = mean, geom = "point", 
+               shape = 18, size = 3, color = "red") +
+  geom_jitter(position = position_jitter(0.2), alpha = 0.5) + 
+  geom_text(
+    data = quartiles,
+    aes(x = "", y = y, label = label),
+    nudge_x = 0.2, hjust = 0
+  ) +
+  theme_bw()
+
+
+
 
 #these ggplot functions are from the tidyverse
 ggplot(snowdata, aes(x=Year , y=Mean_Dec_May_snow_depth)) + geom_point()
